82398ecccc5e7b1dcaaf04613a99dabe1cafbaad
yaseenali512 <yaseenali.bscssef20@iba-suk.edu.pk>
2023-10-28 23:33:24 -0500

    strar rating component added

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StartRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StarRating maxRating={5} />
+  </React.StrictMode>
+);
